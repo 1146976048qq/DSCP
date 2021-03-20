f6f9aa84b4589b91c167ffe0b8084a18e252d6b1
kzhang <1146976048@qq.com>
2021-03-20 16:01:03 +0800

    upda
    Upda

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# DSCP
-Code and dataset for our our paper (ccml'2019）
+### First, the wordVectors.npy file should be combined through:
+```
+cat wordVectors-xa* > wordVectors.npy 
+```
+
+the review info has been concluded in the feature embeddings.
+
+### Then, we can run the baselines or the proposed model through:
+```
+python ../baseline/**.py or python dscp.py
+```
---
+++ attention.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import keras
+# import numpy
+
+class Attention(tf.keras.layers.Layer):
+  def __init__(self, units):
+    """
+    :param units: int, attention 层神经元的个数
+    """
+    super(Attention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units)
+    self.W2 = tf.keras.layers.Dense(units)
+    self.V = tf.keras.layers.Dense(1)
+
+  def call(self, query, values):
+    # hidden shape == (batch_size, hidden size)
+    # hidden_with_time_axis shape == (batch_size, 1, hidden size)
+    # we are doing this to perform addition to calculate the score
+    hidden_with_time_axis = tf.expand_dims(query, 1)
+
+    # score shape == (batch_size, max_length, 1)
+    # we get 1 at the last axis because we are applying score to self.V
+    # the shape of the tensor before applying self.V is (batch_size, max_length, units)
+    score = self.V(tf.nn.tanh(
+        self.W1(values) + self.W2(hidden_with_time_axis)))
+
+    # attention_weights shape == (batch_size, max_length, 1)
+    attention_weights = tf.nn.softmax(score, axis=1)
+
+    # context_vector shape after sum == (batch_size, hidden_size)
+    context_vector = attention_weights * values
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector, attention_weights
